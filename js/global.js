04c58e82aa1acab149c8a102d07a8794dac00582
Kosuke Yoshioka <ctkochan22@gmail.com>
2023-06-07 10:38:22 -0700

    Add copy

--- js/global.js
+++ js/global.js
@@ -5,9 +5,11 @@ $(document).ready(function(){
 
     // Get URL parameters
     const params = getUrlParameters(window.location.search.substring(1));
-    var modal = document.getElementById("myModal");
+    var modal = document.getElementById('myModal');
     if (params['moku'] && params['moku'] === 'plaid') {
         modal.style.display = "block";
+        var messageContainer = $("#modal-message");
+        addMessage(messageContainer, 'plaid');
     }
 
 });
@@ -54,3 +56,19 @@ function contact_pop() {
         $("#contact_info").toggle();
     })
 }
+
+function addMessage(messageContainer, type) {
+    switch (type) {
+        case 'plaid':
+            var plaidMessage = "<p>Dear Plaid,</p><br />"
+                + "<p>I have worked at Expensify, an expense reporting platform, as a full-stack software engineer for the past 6 and a half years. I am proud of my tenure because I feel I helped it grow and overcome a variety of different obstacles. I have worked on numerous projects that have spanned the stack. Additionally, I have worked on many of our third-party integrations, becoming the resident expert at Expensify for Onfido, Stripe, and Plaid.</p><br/>"
+                + "<p>I really appreciated the power of our Plaid integration and have always wanted to learn more behind the magic beyond the curtains. In addition to my experience with Plaid at Expensify. I am building a personal project application, a basic daily spending budget tool, that uses Plaid’s API. The more I work with Plaid’s API, the more I have become impressed, slightly obsessed, and insatiably curious as to how it works.</p><br />"
+                + "<p>I'm deeply passionate about finance, and how applications can leverage a smooth connection to financial institutions to empower a new wave of tools and solutions that did not exist before. I would revel in the opportunity to be part of the team that bring in that new age. I hope you’ll consider me as a candidate!</p>"
+                + "<p>Kosuke</p><br />";
+            console.log(plaidMessage);
+            messageContainer.html(plaidMessage);
+        default:
+            return;
+    }
+}
+
